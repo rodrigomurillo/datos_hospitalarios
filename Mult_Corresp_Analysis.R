c0b117aafbe697ea3657c72a850d1911515028cc
Rodrigo <rodrigo.murillo88@gmail.com>
2017-05-28 01:11:33 -0500

    Actualización MCA

--- Mult_Corresp_Analysis.R
+++ Mult_Corresp_Analysis.R
@@ -7,10 +7,14 @@ db <- db[!is.na(db$CodSexo),]
 db <- db[!is.na(db$CodEscolaridad),]
 db <- db[!is.na(db$CodLeeEscribe),]
 db <- db[!is.na(db$CodArea),]
+db <- db[!is.na(db$Edad_Cut),]
+db <- db[!is.na(db$CodDerechohabienciaRegroup),]
 
-# db$Seguro <- ifelse(db$CodDerechohabiencia == "SEGURO GRATUIDAD" | db$CodDerechohabiencia == "PROSPERA")
 
-cols <-c("CodSexo","CodLeeEscribe","CodEscolaridad","CodDerechohabiencia","CodArea")
+
+# Quite escolaridad
+
+cols <-c("CodDerechohabienciaRegroup","CodArea","Edad_Cut","CodSexo")
 mca <- MCA(db[c(cols)])
 
 cats = apply(db[c(cols)], 2, function(x) nlevels(as.factor(x)))
@@ -25,7 +29,9 @@ ggplot(data = mca_vars_df, aes(x = Dim.1, y = Dim.2, label = rownames(mca_vars_d
   geom_hline(yintercept = 0, colour = "gray70") + 
   geom_vline(xintercept = 0, colour = "gray70") + 
   geom_text(aes(colour = Variable)) + 
-  ggtitle("Grafica de ACM de variables de STUDENT en R con FactoMineR")
+  ggtitle("Grafica de MDS")
+ggsave("/home/rodrigo/Dropbox/Data Science/Estadísitica Multivariada/Datos Hospitalarios/MDS_1.png", plot=last_plot())
+
 
 
 
